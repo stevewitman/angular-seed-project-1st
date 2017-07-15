164703df0453b6cc02b977b0eca92b9d0ed74d9e
Steve Witman <stevewitman@gmail.com>
2017-07-14 23:56:12 -0600

    Pass message into message component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Message } from './messages/message.model'
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styles: [`
-        .author {
-            display: inline-block;
-            font-style: italic;
-            font-size: 12px;
-            width: 80%;
-        }
-        .config {
-            display: inline-block;
-            text-align: right;
-            font-size: 12px;
-            width: 19%;
-        }
-    `]
+
 })
 export class AppComponent {
     
-    message = {
-        content: 'A message',
-        author: 'Bob'
-    }
+    message: Message = new Message('Some message', 'Mark')
 }
